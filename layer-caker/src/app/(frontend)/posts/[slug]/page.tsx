0416f6345be4ec68cfd3ddc74f863a809a790b9f
Simeon Griggs <simeon@hey.com>
2024-08-23 12:17:01 +0100

    complete up to lesson 10

--- layer-caker/src/app/(frontend)/posts/[slug]/page.tsx
+++ layer-caker/src/app/(frontend)/posts/[slug]/page.tsx
@@ -1,21 +1,26 @@
-import { client } from '@/sanity/lib/client'
-import { POST_QUERY } from '@/sanity/lib/queries'
-import { notFound } from 'next/navigation'
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
+import { client } from "@/sanity/lib/client";
+import { POST_QUERY } from "@/sanity/lib/queries";
+import { urlFor } from "@/sanity/lib/image";
 
-type PostIndexProps = { params: { slug: string } }
+type PostIndexProps = { params: { slug: string } };
 
-const options = { next: { revalidate: 60 } }
+const options = { next: { revalidate: 60 } };
 
 export default async function Page({ params }: PostIndexProps) {
-  const post = await client.fetch(POST_QUERY, params, options)
-
-  if (!post) {
-    notFound()
-  }
+  const post = await client.fetch(POST_QUERY, params, options);
 
   return (
-    <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+    <main className="container mx-auto grid grid-cols-1 gap-6 py-12">
+      {post?.mainImage ?
+        <Image
+          className="w-full aspect-[800/400]"
+          src={urlFor(post.mainImage).width(800).height(400).quality(80).auto('format').url()}
+          alt={post?.mainImage?.alt || ''}
+          width="800"
+          height="400"
+        /> : null}
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
