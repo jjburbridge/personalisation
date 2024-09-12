8b2185e394b2eac27ab668e0d5efa4c1c24dd9ac
SimeonGriggs <simeon@hey.com>
2024-09-12 09:52:26 +0100

    add code for CDWAF course

--- layer-caker/README.md
+++
@@ -1,3 +0,0 @@
-## Layer Caker
-
-Code created when completing the [Content driven web application foundations](https://www.sanity.io/learn/course/content-driven-web-application-foundations) course on [Sanity Learn](https://www.sanity.io/learn).
--- layer-caker/src/app/(frontend)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Title } from '@/components/Title'
-
-export default async function Page() {
-  return (
-    <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
-      <Title>Layer Caker Home Page</Title>
-    </section>
-  )
-}
