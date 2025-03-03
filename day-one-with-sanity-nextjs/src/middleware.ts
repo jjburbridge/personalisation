8999be7cc1725858a92491e5663390462d1f9cde
Jon Burbridge <jonathan.burbridge@sanity.io>
2025-03-04 00:36:04 +0100

    broarder middleware

--- day-one-with-sanity-nextjs/src/middleware.ts
+++ day-one-with-sanity-nextjs/src/middleware.ts
@@ -3,13 +3,10 @@ import type { NextRequest } from "next/server";
 import { setCookiesValue } from "./lib/experiments";
 
 export function middleware(request: NextRequest) {
-  console.log("dest header", request.headers.get("sec-fetch-dest"));
   if (request.headers.get("sec-fetch-dest") !== "document") {
-    console.log("middleware not document");
     return;
   }
   let response = NextResponse.next();
-  console.log("middleware", request.url);
   response = setCookiesValue(request, response);
 
   return response;
@@ -17,5 +14,7 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   //only run the middleware on the events pages
-  matcher: ["/events/:path*", "/form"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
